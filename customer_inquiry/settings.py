fb955be7c5676fc8bc7f5aff2324544e0be67aad
Aryan <aryan@aventusinformatics.com>
2022-04-17 16:10:55 +0530

    Automatic configuration with django-heroku

--- customer_inquiry/settings.py
+++ customer_inquiry/settings.py
@@ -123,3 +123,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_heroku
 
 django_heroku.settings(locals())
+
+CSRF_TRUSTED_ORIGINS = ["https://customer-inquiry.herokuapp.com/"]
