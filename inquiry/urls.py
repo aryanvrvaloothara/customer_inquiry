17c76ef5adb4e28d96c479a50de8694c82bb3bc6
Aryan <aryan@aventusinformatics.com>
2022-04-17 15:22:17 +0530

    requirements.txt

--- inquiry/urls.py
+++ inquiry/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from inquiry import views
 
 urlpatterns = [
+    path('', views.verify_mobile_number, name='verify_mobile_number'),
     path('verify_mobile_number/', views.verify_mobile_number, name='verify_mobile_number'),
     path('enter_otp/', views.verify_otp, name='enter_otp'),
     path('inquiry/', views.inquiry, name='inquiry'),
